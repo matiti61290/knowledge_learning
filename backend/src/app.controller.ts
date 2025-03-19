e3266b0e660d88f2ad69f35599fa9e81d8af368e
Mathieu <mathieu.barbey18@gmail.com>
2025-03-19 11:11:22 +0100

    documentation for register controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,10 +5,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  /**
-   * Controller retournant un Hello World
-   * @returns Hello world
-   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- backend/src/auth/register/register.controller.ts
+++ backend/src/auth/register/register.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Post, Body, ValidationPipe, UsePipes, Query, Get } from '@n
 import { RegisterService } from './register.service';
 import { CreateUserDto } from '../../dto/create-user.dto';
 
+/**
+ * Gère les routes de l'inscription
+ */
 @Controller('register')
 export class RegisterController {
 
@@ -9,14 +12,23 @@ export class RegisterController {
         private readonly registerService: RegisterService 
     ){}
 
-    // Route for sign up
+    /**
+     * Gère la requete pour l'inscription.
+     * @param createUserDto 
+     * @returns {Promise<User>}
+     * Voir register.service.ts pour la logique.
+     */
     @Post()
     @UsePipes( new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true}))
     async registrationUser(@Body() createUserDto: CreateUserDto) {
         return this.registerService.registration(createUserDto)
     }
 
-    // Validation account
+    /**
+     * Gère la requête pour vérifier le compte
+     * @param token 
+     * Voir register.service.ts pour la logique.
+     */
     @Get('validation')
     async validateAccount(@Query('token') token: string) {
         await this.registerService.validateUser(token)
