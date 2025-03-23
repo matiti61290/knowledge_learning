ca9c910e1c1d78ad28ff97ef26e6f34d0b67f05b
Mathieu <mathieu.barbey18@gmail.com>
2025-03-23 11:08:03 +0100

    tests for login controller

---
+++ backend/src/auth/login/login.controller.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { LoginService } from './login.service'
+import { LoginController } from './login.controller'
+import { LoginUserDto } from '../../dto/login-user.dto'
+
+describe('LoginController', () => {
+    let loginService : LoginService
+    let loginController: LoginController
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [LoginController],
+            providers:[{
+                provide: LoginService,
+                useValue: {
+                    login: jest.fn()
+                }
+            }]
+        }).compile()
+
+        loginService = module.get<LoginService>(LoginService)
+        loginController = module.get<LoginController>(LoginController)
+    })
+
+    describe('test the login controller', ()  => {
+         it('sould call the login service and return a result', async () => {
+            const loginUserDto = new LoginUserDto()
+            Object.assign(loginUserDto, {
+                mail: 'John.Doe@test.com',
+                password: 'JohnDoe61@'
+            })
+
+
+            const mockToken = { access_token: 'mock-token'} 
+            
+            jest.spyOn(loginService, 'login').mockResolvedValueOnce(mockToken)
+
+            const result = await loginController.login(loginUserDto)
+
+            expect(loginService.login).toHaveBeenCalledWith(loginUserDto)
+            expect(result).toEqual(mockToken)
+         })
+    })
+})
