38da9736ba52273db4fd8d715c8d174ccada2665
Mathieu <mathieu.barbey18@gmail.com>
2025-06-20 15:19:11 +0200

    style lesson page

--- frontend/src/pages/Lesson.js
+++ frontend/src/pages/Lesson.js
@@ -27,16 +27,14 @@ function Lesson ( ) {
     }
 
     return (
-        <div>
-            <h1>{lesson.title}</h1>
-            <div>
-                {lesson.url_video === "undefined" ? (
+        <div className="container">
+            <h1 className="mt-5 fw-bold text-decoration-underline">{lesson.title}</h1>
+            <div className="d-flex flex-column align-items-center">
+                <div className="my-4">
                     <video width={640} height={360} controls>
                         <source src={lesson.url_video} type="video/mp4" />
                     </video>
-                ) : (
-                    <pa>Ya une video</pa>
-                )}
+                </div>
                 <p>{lesson.content}</p>
             </div>
         </div>
