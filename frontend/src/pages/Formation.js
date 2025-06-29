8d83f043fe9b8e2f61274b267eb41c69c279c138
Mathieu <mathieu.barbey18@gmail.com>
2025-06-29 15:18:07 +0200

    correction url API

--- frontend/src/pages/Formation.js
+++ frontend/src/pages/Formation.js
@@ -9,7 +9,7 @@ function Formation() {
    
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACKEND_URL}/${formationId}`, {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/formations/${formationId}`, {
             method: 'GET',
             credentials: 'include'
         })
@@ -26,7 +26,7 @@ function Formation() {
     }, [formationId])
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACKEND_URL}/${formationId}/lessons`, {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/formations/${formationId}/lessons`, {
             method: 'GET',
             credentials: 'include'
         })
